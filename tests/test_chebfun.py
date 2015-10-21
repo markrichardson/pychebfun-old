3c3cc13d8cff13df7dd739ac721d955e81293347
markrichardson <mrichardson82@gmail.com>
2015-10-21 11:48:45 +0100

    Adding a unit-test and associated test data for the functionality added in previous commit

--- tests/test_chebfun.py
+++ tests/test_chebfun.py
@@ -569,6 +569,19 @@ class TestVector(unittest.TestCase):
         assert_close(s[:], s)
 
 
+class TestRoots(unittest.TestCase):
+    """
+    General root-finding tests.
+    """
+    
+    def test_roots_of_flat_function(self):
+        """
+        Check roots() does not fail for extremely flat Chebfuns such 
+        as those representing cumulative distribution funcitons.
+        """
+        cdf = Chebfun.from_data(flat_chebfun_vals, domain=[-0.7, 0.7])
+        npt.assert_allclose((cdf-0.05).roots(), 0.1751682246791747)
+
 # class Test_2D(Test_Chebfun):
 # 	def setUp(self):
 # 		Chebfun.record = True
